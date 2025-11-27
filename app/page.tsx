05225e8b9910ec2ccac8cd803a7a9deef653f6b8
hamid shahsavani <051.sys113@gmail.com>
2025-11-27 13:52:13 +0330

    chore: add product component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,42 @@
+import { ProductCard } from "@/components/product";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <section>
+      <div className="container">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2 2xl:grid-cols-6 gap-4">
+          <ProductCard
+            title="Product 1"
+            price={100}
+            image="/images/product.webp"
+          />
+          <ProductCard
+            title="Product 1"
+            price={100}
+            image="/images/product.webp"
+          />
+          <ProductCard
+            title="Product 1"
+            price={100}
+            image="/images/product.webp"
+          />
+          <ProductCard
+            title="Product 1"
+            price={100}
+            image="/images/product.webp"
+          />
+          <ProductCard
+            title="Product 1"
+            price={100}
+            image="/images/product.webp"
+          />
+          <ProductCard
+            title="Product 1"
+            price={100}
+            image="/images/product.webp"
+          />
+        </div>
+      </div>
+    </section>
   );
 }
---
+++ components/product.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import Image from "next/image";
+import { ShoppingCart } from "lucide-react";
+import { Button } from "@/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/ui/card";
+import { cn } from "@/utils/cn";
+
+interface ProductCardProps {
+  title: string;
+  price: number;
+  image: string;
+}
+
+export const ProductCard = (props: ProductCardProps) => {
+  return (
+    <Card
+      className={cn(
+        "group flex h-full flex-col overflow-hidden transition-all shadow-none border-border duration-300 hover:-translate-y-1"
+      )}
+    >
+      <div className="relative aspect-square overflow-hidden bg-muted">
+        <Image
+          src={props.image}
+          alt={"تصویر"}
+          fill
+          className="object-cover transition duration-500"
+        />
+      </div>
+      <CardHeader className="space-y-2 border-y p-3">
+        <CardTitle className="line-clamp-2 text-base">{props.title}</CardTitle>
+      </CardHeader>
+      <CardFooter className="flex items-center justify-between p-3">
+        <span className="text-lg font-medium text-foreground">
+          {props.price} $
+        </span>
+        <Button className="w-fit">
+          <ShoppingCart className="size-4" />
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
